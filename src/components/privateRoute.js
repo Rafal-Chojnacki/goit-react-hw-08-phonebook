27a041e6b954aa4aa8fe2c58835406f18515a27f
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-08-11 13:50:35 +0200

    Test

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -1,16 +1,16 @@
-import { useNavigate, Route } from 'react-router-dom';
+// import { useNavigate, Route } from 'react-router-dom';
 
-export const PrivateRoute = ({ element, ...rest }) => {
-    const jwtToken = localStorage.getItem('jwtToken');
-    const navigate = useNavigate();
+// export const PrivateRoute = ({ element, ...rest }) => {
+//     const jwtToken = localStorage.getItem('jwtToken');
+//     const navigate = useNavigate();
   
-    if (!jwtToken) {
-      // Redirect to login if JWT is not present
-      navigate('/login');
-      return null;
-    }
+//     if (!jwtToken) {
+//       // Redirect to login if JWT is not present
+//       navigate('/login');
+//       return null;
+//     }
   
-    // Perform JWT verification here, if needed
+//     // Perform JWT verification here, if needed
   
-    return <Route {...rest} element={element} />;
-  };
+//     return <Route {...rest} element={element} />;
+//   };
